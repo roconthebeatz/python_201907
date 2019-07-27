164e03ee0e9bc4952a4588999b8ff4350e126c6b
yongseongCho <44962282+yongseongCho@users.noreply.github.com>
2019-07-27 16:19:57 +0900

    Add files via upload

--- day_07/pandas/pandas_01.py
+++ day_07/pandas/pandas_01.py
@@ -14,12 +14,28 @@ import pandas as pd
 
 # CSV 파일을 로딩하는 예제
 # 1. 파일의 경로를 저장하고 있는 변수의 선언
-fname = './data/scores.csv'
+fname_csv = './data/scores.csv'
 # 2. pandas 모듈의 read_csv 함수를 사용하여
 #   파일을 로딩
-scores = pd.read_csv(fname)
+scores_csv = pd.read_csv(fname_csv)
 
-print(scores)
+print(scores_csv)
+
+# EXCEL 파일을 로딩하는 예제
+# 추가적인 모듈의 설치가 필요!
+# - 엑셀 파일 Read, Write 모듈 설치
+# - pip install xlrd
+# - pip install xlwt
+# - pip install openpyxl
+# - pip install xlrd xlwt openpyxl
+
+# 1. 파일의 경로를 저장하고 있는 변수의 선언
+fname_excel = './data/scores.xlsx'
+# 2. pandas 모듈의 read_csv 함수를 사용하여
+#   파일을 로딩
+scores_excel = pd.read_excel(fname_excel)
+
+print(scores_excel)
 
 
 
